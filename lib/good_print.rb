1377a4723f1c45ee425cf59d4db31c6559fd0d2a
Mark Dodwell <mark@mkdynamic.co.uk>
2014-09-14 00:04:17 -0700

    allow passing IO object

--- lib/good_print.rb
+++ lib/good_print.rb
@@ -7,40 +7,42 @@ module GoodPrint
     def gp_puts(string, level, trailer = false, end_with_comma = false)
       prefix = trailer ? "" : " " * GP_INDENT * level
       suffix = end_with_comma ? "," : ""
-      puts "#{prefix}#{string}#{suffix}"
+      "#{prefix}#{string}#{suffix}\n"
     end
 
     def gp_print(string, level, trailer = false, end_with_comma = false)
       prefix = trailer ? "" : " " * GP_INDENT * level
       suffix = end_with_comma ? "," : ""
-      print "#{prefix}#{string}#{suffix}"
+      "#{prefix}#{string}#{suffix}"
     end
 
-    def gp(object, level = 0, trailer = false, end_with_comma = false)
+    def gp(io, object, level = 0, trailer = false, end_with_comma = false)
       case object
       when Hash
-        gp_puts "{", level, trailer
+        io << gp_puts("{", level, trailer)
         object.each_pair.each_with_index do |(k, v), idx|
-          gp_print "#{k.inspect} => ", level + 1
-          gp v, level + 1, true, idx < object.size - 1
+          io << gp_print("#{k.inspect} => ", level + 1)
+          gp io, v, level + 1, true, idx < object.size - 1
         end
-        gp_puts "}", level, false, end_with_comma
+        io << gp_puts("}", level, false, end_with_comma)
       when Enumerable
         prefix = if (defined? Set) && object.is_a?(Set)
           "<Set>"
         end
-        gp_puts "#{prefix}[", level, trailer
+        io << gp_puts("#{prefix}[", level, trailer)
         object.each_with_index do |v, idx|
-          gp v, level + 1, false, idx < object.size - 1
+          gp io, v, level + 1, false, idx < object.size - 1
         end
-        gp_puts "]", level, false, end_with_comma
+        io << gp_puts("]", level, false, end_with_comma)
       else
-        gp_puts "#{object.inspect}", level, trailer, end_with_comma
+        io << gp_puts("#{object.inspect}", level, trailer, end_with_comma)
       end
+
+      nil
     end
   end
 end
 
-def gp(*args)
-  GoodPrint.gp(*args)
+def gp(object)
+  GoodPrint.gp(STDOUT, object)
 end
